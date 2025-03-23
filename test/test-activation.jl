23ae4fe214015cda9df2dd3a8ca1660783adfb57
SCiarella <simoneciarella@gmail.com>
2025-03-23 08:34:44 +0100

    autopush

--- test/test-activation.jl
+++ test/test-activation.jl
@@ -26,14 +26,8 @@ u = ds(u0)
 N = size(u, 1)
 # Define some activation layers
 # (1) Identity activation
-actlayer_identity = create_CNOactivation(
-    T,
-    D,
-    N,
-    cutoff,
-    activation_function = identity,
-    force_cpu = true,
-)
+actlayer_identity =
+    create_CNOactivation(T, D, N, cutoff, activation_function = identity, force_cpu = true)
 u_identity = actlayer_identity(u)
 # (2) Tanhshrink activation
 actlayer_tanhshrink = create_CNOactivation(
@@ -65,7 +59,7 @@ u_tanhshrink = actlayer_tanhshrink(u)
         @test size(u_tanhshrink) == size(u)
     end
 
-    
+
     # Visualization tests
     @testset "Visualization Tests" begin
         fig = Figure(resolution = (800, 400))
@@ -73,7 +67,7 @@ u_tanhshrink = actlayer_tanhshrink(u)
         heatmap!(ax1, u_identity[:, :, 1, 1])
         ax2 = Axis(fig[1, 2], title = "Tanhshrink Activation on u")
         heatmap!(ax2, u_tanhshrink[:, :, 1, 1])
-    
+
         save("test_figs/activation.png", fig)
         img1 = load("test_figs/activation.png")
         img2 = load("test_figs/activation_baseline.png")
@@ -81,96 +75,59 @@ u_tanhshrink = actlayer_tanhshrink(u)
     end
 
     @testset "Activation AD" begin
-        layers = (x -> actlayer_tanhshrink(x),)
-        closure = Lux.Chain(layers...)
-        rng = Random.Xoshiro(123)
-        θ, st = Lux.setup(rng, closure)
-        θ = ComponentArray(θ)
-
-        # Trigger closure and verify output size
-        out = closure(u, θ, st)
-        @test size(out[1]) == size(u)
-
-        # Gradient calculation test
-        grad = Zygote.gradient(θ -> closure(u, θ, st)[1][1], θ)
-        @test !isnothing(grad)  # Ensure gradient calculation was successful
-        @info "grad is $grad"
-
-
-        # below is the test used for updown: adapt him to above
-        ---------------------
-        x_filter = rand(Float32, 16, 16, 2, 1)
-        result = zeros(Float32, 8, 8, 2, 1)
-        down_factor = 2
-        mydev = Dict("bck" => CPU(), "workgroupsize" => 64, "T" => Float32)
-        downsample_kernel(mydev, x_filter, down_factor, 16)
+        result = actlayer_tanhshrink(u)
         @test sum(result) !== 0.0
 
-        y, back = Zygote.pullback(downsample_kernel, mydev, x_filter, 16, down_factor)
-        x_filter_bar = zeros(Float32, size(x_filter))
-        result_bar = rand(Float32, size(result))
-        _, x_filter_bar, _, _ = back(result_bar)
-        filtered_size = (((1:down_factor:16) for _ = 1:D)..., :, :)
-        redown = x_filter_bar[filtered_size...]
-        @test redown == result_bar
+        y, back = Zygote.pullback(actlayer_tanhshrink, u)
+        @test y == result
+        y_bar = rand(Float32, size(u))
+        x_bar = zeros(Float32, size(u))
+        x_bar = back(y_bar)
+        @test sum(x_bar) !== 0.0
+        @test x_bar != y_bar
     end
 
 end
 
-#@testset "CNO Activation (GPU)" begin
-#
-#    if !CUDA.functional()
-#        @error "CUDA is not functional, skipping test"
-#        return
-#    end
-#
-#    # Setup initial image and parameters
-#    N0 = 512
-#    T = Float32
-#    D = 2
-#    u0 = zeros(T, N0, N0, D, 1)
-#    u0[:, :, 1, 1] .= testimage("cameraman")
-#    u0 = CuArray(u0)
-#    cutoff = 0.1
-#
-#    # Test initial image dimensions
-#    @test size(u0) == (N0, N0, D, 1)
-#
-#    # Downsize the input
-#    down_factor = 6
-#    ds = create_CNOdownsampler(T, D, N0, down_factor, cutoff)
-#    u = ds(u0)
-#    N = size(u, 1)
-#
-#    # Test downsampled size
-#    @test size(u) == (N, N, D, 1)
-#    @test N - 1 == div(N0, down_factor)
-#
-#    # Test create_CNOactivation with identity activation
-#    al_identity = create_CNOactivation(T, D, N, cutoff, activation_function = identity)
-#    u_identity = al_identity(u)
-#    @test size(u_identity) == size(u)
-#
-#    # Test create_CNOactivation with tanhshrink activation
-#    al_tanhshrink = create_CNOactivation(T, D, N, cutoff, activation_function = tanhshrink)
-#    u_tanhshrink = al_tanhshrink(u)
-#    @test size(u_tanhshrink) == size(u)
-#
-#    # Differentiability Test
-#    layers = (x -> al_tanhshrink(x),)
-#    closure = Lux.Chain(layers...)
-#    rng = Random.Xoshiro(123)
-#    θ, st = Lux.setup(rng, closure)
-#    dev = Lux.gpu_device()
-#    θ = ComponentArray(θ)
-#
-#    # Trigger closure and verify output size
-#    out = closure(u, θ, st)
-#    @test size(out[1]) == size(u)
-#
-#    # Gradient calculation test
-#    grad = Zygote.gradient(θ -> closure(u, θ, st)[1][1], θ)
-#    @test !isnothing(grad)  # Ensure gradient calculation was successful
-#
-#end
-#
+# Prepare for GPU tests
+u = CuArray(u)
+actlayer_identity = create_CNOactivation(T, D, N, cutoff, activation_function = identity)
+u_identity = actlayer_identity(u)
+actlayer_tanhshrink =
+    create_CNOactivation(T, D, N, cutoff, activation_function = tanhshrink)
+u_tanhshrink = actlayer_tanhshrink(u)
+
+@testset "CNO Activation (GPU)" begin
+
+
+    @testset "Initial Image Dimensions" begin
+        @test size(u0) == (N0, N0, D, 1)
+        @test size(u) == (N, N, D, 1)
+        @test N == div(N0, down_factor)
+    end
+
+
+    @testset "Identity activation" begin
+        @test size(u_identity) == size(u)
+    end
+
+    # Test create_CNOactivation with tanhshrink activation
+    @testset "Tanhshrink activation" begin
+        @test size(u_tanhshrink) == size(u)
+    end
+
+
+    @testset "Activation AD" begin
+        result = actlayer_tanhshrink(u)
+        @test sum(result) !== 0.0
+
+        y, back = Zygote.pullback(actlayer_tanhshrink, u)
+        @test y == result
+        y_bar = rand(Float32, size(u))
+        x_bar = zeros(Float32, size(u))
+        x_bar = back(y_bar)
+        @test sum(x_bar) !== 0.0
+        @test x_bar != y_bar
+    end
+
+end
